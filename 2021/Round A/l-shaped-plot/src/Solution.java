9b51aff8b5c4b5678bceaa116a37b7b4ed14bd7d
piotr.konopacki <konopackipio@gmail.com>
2023-01-14 17:02:05 +0000

    Adding method for finding "good segment"

--- 2021/Round A/l-shaped-plot/src/Solution.java
+++ 2021/Round A/l-shaped-plot/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068c509
@@ -15,7 +17,8 @@ public class Solution {
                 if(r == 1 || c == 1) {
                     System.out.println("Case #" + i + ": 0" );
                 }
-                int[][] arr = createArray(r, c, sc);         
+                int[][] arr = createArray(r, c, sc);
+                List<List<Solution.Point>> goodSegmentList = findGoodSegment(arr);
 
             }
             System.out.println("end");
@@ -29,8 +32,38 @@ public class Solution {
                 result[i][j] = sc.nextInt();
             }
         }
-
         return result;
+    }
+
+    private static List<List<Point>> findGoodSegment(int[][] grid) {
+        List<List<Point>> goodSegmentsList = new ArrayList<>();
+
+        // Going along X -> axis
+        for(int i = 0; i < grid[0].length; i++) {
+            for(int j = 0; j < grid.length; j++) {
+                List<Point> goodSegment = new ArrayList<>();
+                for (int k = j; k < grid.length; k++) {
+                    if(grid[k][i] == 1) {
+                        goodSegment.add(new Point(k, i));
+                        if(goodSegment.size() > 1) {
+                            goodSegmentsList.add(new ArrayList<>(goodSegment));
+                        }
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return goodSegmentsList;
+    }
+
+    private static class Point {
+        private int x,y;
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 
     
